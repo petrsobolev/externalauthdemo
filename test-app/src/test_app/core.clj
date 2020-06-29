90e9b843be1e3516d89039bd5742f41f00a18296
Petr <petrsobolev1432@gmail.com>
2020-06-29 10:45:39 +0300

    --initial commit : create lein app

---
+++ test-app/src/test_app/core.clj
@@ -0,0 +1,14 @@
+(ns test-app.core
+    (:require [ring.adapter.jetty :as webserver]))
+(defn -main
+    "A very simple web server using Ring & Jetty"
+    [port-number]
+    (webserver/run-jetty
+        (fn [request]
+            {:status  200
+             :headers {}
+             :body    "<h1>Hello, Clojure World</h1>
+                 <p>Welcome to your first Clojure app.
+                 This message is returned regardless of the request, sorry</p>"})
+        {:port  (Integer. port-number)
+         :join? false}))
